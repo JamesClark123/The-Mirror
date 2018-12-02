00175670150290f268b28c592d2c59ed2b4fbd8b
BC Swift Student Loan 1 <bcswiftstudentloan1@csomswiftloan1.local>
2018-12-02 16:43:12 -0500

    Initial Commit

---
+++ The Mirror/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Mirror
+//
+//  Created by BC Swift Student Loan 1 on 12/2/18.
+//  Copyright © 2018 BC Swift Student Loan 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
